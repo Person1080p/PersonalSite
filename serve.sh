87ef6380187805f54baa62707795e625394c3e02
Christian Gutierrez <Person1080p@gmail.com>
2022-05-17 19:57:19 -0400

    notes for webserver container

--- serve.sh
+++ serve.sh
@@ -7,3 +7,7 @@ scp -r * web@192.168.1.166:/var/www/html/
 git add ./
 git commit -m "$1"
 git push
+
+---
+turn off x11: systemctl set-default multi-user.target
+enable x11: systemctl set-default graphical.target

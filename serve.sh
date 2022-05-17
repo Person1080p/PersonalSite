6f1035491dc96793f501f7462eb32b3bee832c67
Christian Gutierrez <Person1080p@gmail.com>
2022-05-17 19:27:54 -0400

    new setup for contanierizing servers using proxmox hypervisor

--- serve.sh
+++ serve.sh
@@ -1,4 +1,9 @@
-cp -R * /usr/share/nginx/html
+# cp -R * /usr/share/nginx/html
+# git add ./
+# git commit -m "$1"
+# git push
+
+scp -r * web@192.168.1.166:/var/www/html/
 git add ./
 git commit -m "$1"
 git push
